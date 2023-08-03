fcaeaec23187c1894a368e88a34032b881f7cd25
shinoki <endernasha85@gmail.com>
2023-08-03 16:57:44 +0300

    redirect fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Content from '@/screen/content/content';
 import Menu from '@/screen/menu/menu';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
 	// TODO: temprry .main-mobile-overflow
 	// h-screen
+
+	redirect('/a');
+
 	return (
 		<main className="grid grid-cols-grid-side-medium h-[100dvh] main-mobile-overflow">
 			<Menu />
