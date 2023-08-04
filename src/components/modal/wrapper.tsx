918af138103c4758fe19f96bb35a6d8fd8b0c69f
shinoki <endernasha85@gmail.com>
2023-08-05 01:45:15 +0300

    mobile ui fixes

--- src/components/modal/wrapper.tsx
+++ src/components/modal/wrapper.tsx
@@ -8,7 +8,7 @@ interface IWrapper {
 export const Wrapper: React.FC<IWrapper> = ({ children, showHandler }) => {
 	return (
 		<div
-			className="absolute left-0 top-0 z-[100] w-screen h-screen bg-[#00000040]
+			className="absolute left-0 top-0 z-[100] w-screen h-[100dvh] bg-[#00000040]
             flex items-center justify-center modal-wrapper"
 			onClick={(e: any) =>
 				e.target?.classList.contains('modal-wrapper')
