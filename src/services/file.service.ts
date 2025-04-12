d2d0509809c2bfb34c495e5e46d2a8cb4c12dde2
ShinokiX3 <endernasha85@gmail.com>
2025-04-12 13:57:12 +0300

    add: service for sending files

---
+++ src/services/file.service.ts
@@ -0,0 +1,53 @@
+import { IUser } from '@/store/user/user.types';
+import { SOURCE } from './sources.const';
+
+export interface SendFileMessage {
+    formData: FormData;
+    token: string;
+}
+
+export interface SendFileMessageResponse {
+    userId: string,
+    messageId: string,
+    message: {
+        pictures: Array<string>,
+        message: string
+    },
+    writed: string,
+    read: string
+}
+
+export interface SendFileMessagePromise {
+    body: ReadableStream;
+    bodyUsed: boolean;
+    headers: Headers
+    ok: boolean;
+    redirected: boolean;
+    status: number;
+    statusText: string;
+    type: string;
+    url: string;
+}
+
+class FileService {
+    async sendFileMessage({ formData, token }: SendFileMessage): Promise<Response> {
+        try {
+            const response = await fetch(`${SOURCE}/chat/send/photo`,
+                {
+                    method: 'POST',
+                    mode: 'cors',
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                    body: formData,
+                }
+            );
+
+            return response;
+        } catch (error) {
+            throw new Error();
+        }
+    }
+}
+
+export default new FileService();
